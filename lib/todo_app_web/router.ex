b352ec3fea62f0460223dee36ee67cead77bdd89
ricardopu1993 <ricardopu1993@gmail.com>
2022-09-21 20:55:24 -0500

    Adición de routes dentro de file router.ex

--- lib/todo_app_web/router.ex
+++ lib/todo_app_web/router.ex
@@ -1,6 +1,8 @@
 defmodule TodoAppWeb.Router do
   use TodoAppWeb, :router
 
+  import TodoAppWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule TodoAppWeb.Router do
     plug :put_root_layout, {TodoAppWeb.LayoutView, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
@@ -16,15 +19,15 @@ defmodule TodoAppWeb.Router do
 
   scope "/", TodoAppWeb do
     pipe_through :browser
-
     get "/", PageController, :index
+    resources "/tasks", TaskController
+    resources "/lists", ListController
   end
-
   # Other scopes may use custom stacks.
-  # scope "/api", TodoAppWeb do
-  #   pipe_through :api
-  # end
-
+  scope "/api", TodoAppWeb do
+    pipe_through :api
+    get "/tasks", API.TaskController, :index
+  end
   # Enables LiveDashboard only for development
   #
   # If you want to use the LiveDashboard in production, you should put
@@ -34,14 +37,11 @@ defmodule TodoAppWeb.Router do
   # as long as you are also using SSL (which you should anyway).
   if Mix.env() in [:dev, :test] do
     import Phoenix.LiveDashboard.Router
-
     scope "/" do
       pipe_through :browser
-
       live_dashboard "/dashboard", metrics: TodoAppWeb.Telemetry
     end
   end
-
   # Enables the Swoosh mailbox preview in development.
   #
   # Note that preview only shows emails that were sent by the same
@@ -49,8 +49,40 @@ defmodule TodoAppWeb.Router do
   if Mix.env() == :dev do
     scope "/dev" do
       pipe_through :browser
-
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", TodoAppWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    get "/users/register", UserRegistrationController, :new
+    post "/users/register", UserRegistrationController, :create
+    get "/users/log_in", UserSessionController, :new
+    post "/users/log_in", UserSessionController, :create
+    get "/users/reset_password", UserResetPasswordController, :new
+    post "/users/reset_password", UserResetPasswordController, :create
+    get "/users/reset_password/:token", UserResetPasswordController, :edit
+    put "/users/reset_password/:token", UserResetPasswordController, :update
+  end
+
+  scope "/", TodoAppWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    get "/users/settings", UserSettingsController, :edit
+    put "/users/settings", UserSettingsController, :update
+    get "/users/settings/confirm_email/:token", UserSettingsController, :confirm_email
+  end
+
+  scope "/", TodoAppWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+    get "/users/confirm", UserConfirmationController, :new
+    post "/users/confirm", UserConfirmationController, :create
+    get "/users/confirm/:token", UserConfirmationController, :edit
+    post "/users/confirm/:token", UserConfirmationController, :update
   end
 end
