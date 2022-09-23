ee1d68c183842dc6a53303f97ccc5c96c66d9e94
ricardopu1993 <ricardopu1993@gmail.com>
2022-09-22 20:16:00 -0500

    Add folder fixtures

---
+++ test/support/fixtures/todos_fixtures.ex
@@ -0,0 +1,36 @@
+defmodule TodoApp.TodosFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `TodoApp.Todos` context.
+  """
+
+  @doc """
+  Generate a task.
+  """
+  def task_fixture(attrs \\ %{}) do
+    {:ok, task} =
+      attrs
+      |> Enum.into(%{
+        description: "some description",
+        done: true
+      })
+      |> TodoApp.Todos.create_task()
+
+    task
+  end
+
+  @doc """
+  Generate a list.
+  """
+  def list_fixture(attrs \\ %{}) do
+    {:ok, list} =
+      attrs
+      |> Enum.into(%{
+        name: "some name",
+        tags: []
+      })
+      |> TodoApp.Todos.create_list()
+
+    list
+  end
+end
